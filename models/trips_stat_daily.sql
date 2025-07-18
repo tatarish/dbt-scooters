db3124f6681065e6ce4525db33102f4c5e50311c
Vladislav Tatarnikov <tatvladislav@gmail.com>
2025-07-18 14:51:46 +0300

    mission5_1

--- models/trips_stat_daily.sql
+++ models/trips_stat_daily.sql
@@ -1,10 +1,10 @@
 select
-    date(started_at) as date,
+    date,
     count(*) as trips,
-    max(price) / 100 as max_price_rub,
-    avg(distance) / 1000 as avg_distance_km
+    max(price_rub) as max_price_rub,
+    avg(distance_m) / 1000 as avg_distance_km
 from
-    scooters_raw.trips
+    {{ ref("trips_prep")}}
 group by
     1
 order by
